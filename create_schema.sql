4508b275ef74164203eb6291f189705c2e469d85
aldiyen <aldiyen@gmail.com>
2014-11-15 21:54:55 -0500

    Working URL shortener code which also logs the redirect activity. Still need a way to insert rows via the web

--- create_schema.sql
+++ create_schema.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE url (
     url_id BIGSERIAL PRIMARY KEY,
     -- this blog says TEXT and VARCHAR are stored the same under the hood, and TEXT has no max len: http://blog.jonanin.com/2013/11/20/postgresql-char-varchar/
@@ -8,7 +9,7 @@ CREATE TABLE url (
 CREATE UNIQUE INDEX url_unique_idx ON url ( url );
 
 CREATE TABLE url_access (
-    url_view_id BIGSERIAL PRIMARY KEY,
+    url_access_id BIGSERIAL PRIMARY KEY,
     url_id      BIGINT NOT NULL REFERENCES url(url_id),
     datetime    TIMESTAMP NOT NULL,
     ip_address  VARCHAR(39) NOT NULL -- 39 chars max for IPv6
--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,23 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 module Main where
 
+-- Library imports
 import           Control.Applicative
 import           Control.Lens.TH
-import           Data.Time
+import           Data.Time.Clock
 import           Data.Maybe
+import           GHC.Int
 import           Snap
 import           Snap.Core
-import           Snap.Util.FileServe
 import           Snap.Http.Server
 import           Snap.Snaplet.PostgresqlSimple
+import           Snap.Util.FileServe
 import qualified Data.ByteString.Char8 as B
 
+-- Local imports
 import Converter
 
 data App = App
@@ -20,43 +25,55 @@ data App = App
         _db   :: Snaplet Postgres
     }
 
+-- Magic!
 makeLenses ''App
 
---instance HasPostgres (Handler App App) where
---    getPostgresState = with db get
-
+-- Not a lot of routes here, since we only really forward people
 routes :: [(B.ByteString, Handler App App ())]
 routes = [ ("/", indexHandler),
            ("/", linkHandler)
          ]
 
+-- Magic happens here
 appInit :: SnapletInit App App
 appInit = makeSnaplet "ald.li" "ald.li URL shortener" Nothing $ do
     d <- nestSnaplet "db" db $ pgsInit
     addRoutes routes
     return $ App d
 
-indexHandler :: Handler App App ()
-indexHandler = ifTop $ writeText "Sure..."
+-- So we don't have to call with db all the time
+instance HasPostgres (Handler b App) where
+    getPostgresState = with db get
 
--- Function to load everything
+-- Should probably replace this with some HTML page, eh?
+indexHandler :: Handler App App ()
+indexHandler = ifTop $ writeText "This is a URL shortener. Why are you here?"
+
+-- The main request handling logic:
+-- 1. Treat the URI path as the encoded url ID
+-- 2. If the URI path can be decoded, load the corresponding url_id from DB
+-- 3. If the url_id existed, log that the user visited it back to the DB, then forward them on
+-- FIXME should probably have 400 and 404 handlers
 linkHandler :: Handler App App ()
 linkHandler = do
     request <- getRequest
     case (decode $ rqPathInfo request) of
         Nothing -> modifyResponse $ setResponseStatus 400 "Invalid URI"
-        Just v  -> load v >>= parse
+        Just v  -> loadUrl v >>= (forwardTo request v)
     where decode        = decodeR . B.unpack
-          load x        = with db $ query "SELECT url, url_id FROM url WHERE url_id = ?" [x]
-          -- HEY WTF HOW CAN THIS WORK??? ALSO HOW CAN I GET THE DATE FROM IO -- I CAN USE getCurrentTime or getUtcTime or something like that from Data.Time.Clock, but it's IO UTCTime and I want a damn UTCTime
-          parse :: MonadSnap m => [(B.ByteString, Int)] -> m ()
-          parse [(url, id)] = getRequest >>= (\req -> (logRedirect req id) >> redirect url)
-          parse _       = modifyResponse $ setResponseCode 404
-          logRedirect request id = with db $ execute "INSERT INTO url_access (url_id, datetime, ip_address) VALUES (?, ?, ?)" [id, "2014-01-01", "test"]
---          parse [[url]] = do logError ("Resolved to url: " `B.append` url)
---                             redirect url
---          parse x       = do logError ("Resolved to x: " `B.append` (B.pack $ show x))
---                             modifyResponse $ setResponseCode 404
+          forwardTo request id [Only url] = (logRedirect (rqRemoteAddr request) id) >>= (\_ -> redirect url)
+          forwardTo _       _  _       = modifyResponse $ setResponseCode 404
 
+-- Helper function to load a URL from the DB
+loadUrl :: (HasPostgres m) => Int -> m [Only B.ByteString]
+loadUrl x = query "SELECT url FROM url WHERE url_id = ?" [x]
+
+-- Helper function to insert a row into url_access, recording who we sent where
+logRedirect :: (HasPostgres m) => B.ByteString -> Int -> m Int64
+logRedirect clientIp urlId = do
+    currentTime <- liftIO getCurrentTime
+    execute "INSERT INTO url_access (ip_address, datetime, url_id) VALUES (?, ?, ?)" (clientIp, currentTime, urlId)
+
+-- You know, a main function...
 main :: IO ()
 main = serveSnaplet defaultConfig appInit
